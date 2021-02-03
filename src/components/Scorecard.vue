0070b9aa896c2dcb45a451da00cc43fc4bf1f22e
alexa.kearns <alexa.kearns@potatolondon.com>
2021-02-03 08:31:14 +0000

    Add Tenth frame component and test file

--- src/components/Scorecard.vue
+++ src/components/Scorecard.vue
@@ -3,16 +3,19 @@
   <h2>{{ str }}</h2>
   <Frame str="This is Frame's props" />
   <Frame str="Another Frame prop" />
+  <TenthFrame str="Tenth Frame prop" />
   </div>
 </template>
 
 <script>
 import Frame from './Frame.vue';
+import TenthFrame from './TenthFrame.vue'
 
 export default {
   name: "Scorecard",
   components: {
     Frame,
+    TenthFrame,
   },
   props: {
     str: String
---
+++ src/components/TenthFrame.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="tenth-frame">
+  <h2>10th Frame</h2>
+  <h4>Inside 10th Frame</h4>
+  <h1>{{ score }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TenthFrame',
+  props: {
+    score: Number
+  },
+
+}
+</script>
+
+<style>
+  .tenth-frame {
+    border : double  5px orange;
+  }
+</style>
